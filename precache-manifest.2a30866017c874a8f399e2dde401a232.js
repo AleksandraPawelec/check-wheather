09c0744715e88894c5e888814dc40c030b1ed136
Aleksandra Pawelec <58439839+AleksandraPawelec@users.noreply.github.com>
2020-02-05 15:36:57 +0100

    Updates

---
+++ precache-manifest.2a30866017c874a8f399e2dde401a232.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e7f989a7a04adcedd66",
+    "url": "/check-wheather/./static/css/main.e5a14c56.chunk.css"
+  },
+  {
+    "revision": "9e7f989a7a04adcedd66",
+    "url": "/check-wheather/./static/js/main.0ab08579.chunk.js"
+  },
+  {
+    "revision": "2bd9909d514c7f3dad45",
+    "url": "/check-wheather/./static/js/runtime~main.12e25e85.js"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/check-wheather/./static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "1405efa02992e58519a6b0c181646ea7",
+    "url": "/check-wheather/./index.html"
+  }
+];
